00dfbb1c6c95c8b73c0156e703f7918fe6da821b
Rémi Andrieux <pimolo10300@gmail.com>
2016-05-03 01:44:25 +0200

    Change PlacesModel to DTO (remove accessors)

--- src/UnderDevCrew/Bundle/PlacesApiBundle/Factory/PlacesModelFactory.php
+++ src/UnderDevCrew/Bundle/PlacesApiBundle/Factory/PlacesModelFactory.php
@@ -2,7 +2,7 @@
 
 namespace UnderDevCrew\Bundle\PlacesApiBundle\Factory;
 
-use UnderDevCrew\Bundle\Model\PlacesModel;
+use UnderDevCrew\Bundle\PlacesApiBundle\Model\PlacesModel;
 
 class PlacesModelFactory
 {
@@ -12,16 +12,19 @@ class PlacesModelFactory
      */
     public function create(array $dataFromApi)
     {
+        $model = new PlacesModel();
+
         $result = $dataFromApi['result'];
-        return (new PlacesModel())
-            ->setFullAddress($result['formatted_address'])
-            ->setLatitude($result['geometry']['location']['lat'])
-            ->setLongitude($result['geometry']['location']['lng'])
-            ->setMapsUrl($result['url'])
-            ->setUrl($result['website'])
-            ->setNote($result['rating'])
-            ->setShortAddress($result['vicinity'])
-            ->setPhoneNumber($result['international_phone_number'])
-        ;
+
+        $model->fullAddress = $result['formatted_address'];
+        $model->latitude = $result['geometry']['location']['lat'];
+        $model->longitude = $result['geometry']['location']['lng'];
+        $model->mapsUrl = $result['url'];
+        $model->url = $result['website'];
+        $model->note = $result['rating'];
+        $model->shortAddress = $result['vicinity'];
+        $model->phoneNumber = $result['international_phone_number'];
+
+        return $model;
     }
 }
